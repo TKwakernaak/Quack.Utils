ff0f764403ee86ae990d53ff700cebc1c37dfc7d
thijs.kwakernaak <Thijs.Kwakernaak@sunwebgroup.com>
2020-06-23 17:17:22 +0200

    Added extension to wrap validatable objects

---
+++ Quack.Validation/WrapExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Quack.Validation.Contracts;
+
+namespace Quack.Validation
+{
+    public static class WrapExtensions
+    {
+
+        /// <summary>
+        ///  Wrap T into ValidatableObject<typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="rules"></param>
+        public static ValidatableObject<T> Wrap<T>(this T input, params IValidationRule<T>[] rules)
+        {
+            var validatableObject = new ValidatableObject<T>();
+            validatableObject.Value = input;
+
+            if (rules != null)
+            {
+                foreach (var rule in rules)
+                {
+                    validatableObject.AddValidation(() => rule);
+                }
+            }
+
+            return validatableObject;
+        }
+    }
+}
+
