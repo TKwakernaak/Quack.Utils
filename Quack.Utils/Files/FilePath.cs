d2efd33107e10ac38bce2431cf846ed34cf42698
Thijs Kwakernaak <Thijs.Kwakernaak@sunwebgroup.com>
2021-03-18 19:17:43 +0100

    Add filewatcher

---
+++ Quack.Utils/Files/FilePath.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Sundio.Player.Sidecar.Domain.Models.PriceReport
+{
+    /// <summary>
+    /// provides a simple abstraction over file paths.
+    /// </summary>
+    public class FilePath
+    {
+        public FilePath(string filePath)
+        {
+            if (!IsValidFilePath(filePath))
+            {
+                throw new ArgumentException($"{filePath} is not a valid file path");
+            }
+
+            FullPath = filePath;
+        }
+
+        public string FullPath { get; }
+
+        public string FileName => Path.GetFileName(FullPath);
+
+        /// <summary>
+        /// Directory where the file represented by this class is located
+        /// </summary>
+        public string FolderPath => Path.GetDirectoryName(FullPath);
+
+        public override string ToString()
+        {
+            return FullPath;
+        }
+
+        public static bool IsValidFilePath(string filePath)
+        {
+            return !string.IsNullOrEmpty(filePath) && Path.HasExtension(filePath);
+        }
+
+        public void EnsureFileExtensionOf(string extension)
+        {
+            if (!Path.GetExtension(FullPath).Contains(extension, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException($"destination file path {FullPath} does not have the expected extension {extension}");
+            }
+        }
+
+        public void EnsureFileExists()
+        {
+            if (!File.Exists(FullPath))
+            {
+                throw new InvalidOperationException($"file path {FullPath} does not exist.");
+            }
+        }
+    }
+}
---
+++ Quack.Utils/Files/FileWatcher.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using System.IO;
+using Sundio.Player.Sidecar.Domain.Models.PriceReport;
+using System;
+
+namespace Quack.Utils.Files
+{
+    /// <summary>
+    /// Simple abstraction over microsofts filewatcher to make it awaitable using TaskCompletionSource
+    /// </summary>
+    public class FileWatcher
+    {
+        /// <summary>
+        /// Manually controlled task
+        /// </summary>
+        private readonly TaskCompletionSource<FilePath> _fileCreatedTsc = new TaskCompletionSource<FilePath>();
+        private readonly FileSystemWatcher _fileSystemWatcher;
+
+        private readonly TimeSpan _timeOutAfter;
+
+        public FileWatcher(TimeSpan timeOutAfter, FileSystemWatcher fileSystemWatcher)
+        {
+            _timeOutAfter = timeOutAfter;
+            _fileSystemWatcher = fileSystemWatcher;
+        }
+
+        /// <summary>
+        /// Wait for a file to be created at the specified path, which adheres to the specified parameters
+        /// </summary>
+        public async Task<FilePath> WaitForCreatedFileAsync()
+        {
+            _ = Task.Run(() => WaitForChangeOn());
+            _ = Task.Run(() => HandleTimeout());
+
+            return await _fileCreatedTsc.Task.ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Watch the specified folder for changes which adhere to the passed filter.
+        /// </summary>
+        private void WaitForChangeOn()
+        {
+            _fileSystemWatcher.Created += Watcher_OnFileCreated;
+            _fileSystemWatcher.Error += Watcher_OnError;
+        }
+
+        private void Watcher_OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            var file = new FilePath(e.FullPath);
+            _fileCreatedTsc.SetResult(file);
+        }
+
+        private void Watcher_OnError(object sender, ErrorEventArgs e)
+        {
+            var ex = new InvalidOperationException($"error while waiting for a new file to be created {e?.GetException()?.Message}", e.GetException());
+            _fileCreatedTsc.SetException(ex);
+        }
+
+        private async Task HandleTimeout()
+        {
+            var end = DateTime.UtcNow.Add(_timeOutAfter);
+
+            while (DateTime.UtcNow < end)
+            {
+                if (_fileCreatedTsc.Task.IsCompleted || _fileCreatedTsc.Task.IsFaulted)
+                {
+                    break;
+                }
+
+                await Task.Delay(2000).ConfigureAwait(false);
+            }
+            if (!_fileCreatedTsc.Task.IsCompleted || !_fileCreatedTsc.Task.IsCanceled)
+            {
+                _fileCreatedTsc.SetCanceled();
+            }
+        }
+
+        public void Dispose()
+        {
+            _fileSystemWatcher?.Dispose();
+            _fileCreatedTsc?.Task?.Dispose();
+        }
+    }
+}
